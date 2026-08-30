7e3db97ad5335fe87f07c6206f10e39fb3993363
israrahmedattari <146367827+israrahmedattari@users.noreply.github.com>
2026-08-30 11:04:24 +0500

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ st.set_page_config(
 # ============================================================
 
 # Define trial expiration date (Year, Month, Day) -> Next Saturday
-TRIAL_EXPIRATION_DATE = date(2026, 8, 30)
+TRIAL_EXPIRATION_DATE = date(2026, 9, 5)
 
 
 def show_404():
